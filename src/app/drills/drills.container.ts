95c2ae27b306e7bfa2bdfeccde059279078aa29a
m3enjamin <m3enjamin@gmail.com>
2018-02-25 18:55:32 +0100

    Removed material tabs

--- src/app/drills/drills.container.ts
+++ src/app/drills/drills.container.ts
@@ -1,11 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Drill, Aspect } from '@app/shared/model';
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-drills',
-  template: ``
+  template: `
+  <app-drills-list [drills]="drills | async"></app-drills-list>
+  `
 })
-export class DrillsContainer implements OnInit {
-  constructor() {}
+export class DrillsContainer {
+  private drillCollection: AngularFirestoreCollection<Drill>;
+  private aspectCollection: AngularFirestoreCollection<Aspect>;
+  drills: Observable<Drill[]>;
+  aspects: Observable<Aspect[]>;
 
-  ngOnInit() {}
+  constructor(private afs: AngularFirestore) {
+    this.drillCollection = afs.collection<Drill>('drills');
+    this.aspectCollection = afs.collection<Aspect>('aspects');
+    this.drills = this.drillCollection.snapshotChanges().map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data() as Drill;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      });
+    });
+    this.aspects = this.aspectCollection.snapshotChanges().map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data() as Aspect;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      });
+    });
+  }
+
+  createDrill(drill: Drill) {
+    this.drillCollection.add(drill);
+  }
+
+  removeDrill(drill: Drill) {
+    this.afs.doc('drills/' + drill.id).delete();
+  }
 }
